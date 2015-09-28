9a25d2277e6f53ff9bc83f7eecb3893e3d7eb727
joshuarossi <joshua.rossi@icloud.com>
2015-09-28 17:52:15 -0500

    added ws requirement as well as gitignore

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by joshuarossi on 9/28/15.
+ */
+
+module.exports = {
+    websocket: function() {
+        return null;
+    },
+
+    rest: function() {
+        return null;
+    }
+};
