a4c217011a5a3740092f429f5e31295cf0f9f0b6
joshuarossi <joshua.rossi@icloud.com>
2015-09-28 23:59:25 -0500

    changed the github url

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
  * Created by joshuarossi on 9/28/15.
  */
-WebSocket = require('ws');
+var WebSocket = require('ws');
 module.exports = {
     websocket: function() {
         var key = '';
         var secret = '';
-        ws = new WebSocket("wss://api2.bitfinex.com:3001/ws");
+        var ws = new WebSocket("wss://api2.bitfinex.com:3001/ws");
         return ws;
     },
 
