667636f121a6a89bb7a3cb8dec9e5ef0b85413e0
joshuarossi <joshua.rossi@icloud.com>
2015-10-23 03:16:41 -0500

    on the right track, the flow is bitfinex = new BFX(api_key, api_secret) bitfinex.rest.ticker() bitfinex.ws.subTicker()

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect,
     BFX = require('../index');
 
-describe('Array', function() {
+describe('Loading Module', function() {
     describe('#BFX', function () {
         it('should be loaded', function () {
             expect(BFX).to.be.a.function;
