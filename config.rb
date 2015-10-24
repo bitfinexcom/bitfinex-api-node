673ad850af66d72410605638d257ac6ee8fcfe8f
joshuarossi <joshua.rossi@icloud.com>
2015-10-23 22:22:16 -0500

    added .istanbul.yml

--- config.rb
+++ config.rb
@@ -14,6 +14,7 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
+set :coverage_dir, 'coverage'
 
 # Activate the syntax highlighter
 activate :syntax
