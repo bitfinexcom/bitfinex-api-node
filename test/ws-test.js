c08adeeeafd833eb90d1b3113c20833f9f2d26e8
Yago <yago.ftw@gmail.com>
2015-12-12 20:48:33 +0100

    Refactor BitfinexWS

--- test/ws-test.js
+++ test/ws-test.js
@@ -1,180 +1,191 @@
-var expect = require('chai').expect,
-    BFX = require('../index'),
-    _ = require('lodash'),
-    test_keys = require('./keys.json');
+"use strict";
 
-bfx = new BFX(test_keys.standard.api_key, test_keys.standard.api_secret);
-var bfx_ws = bfx.ws;
-describe('Websocket', function () {
-    this.timeout(30000);
-    it('subscribing',
-        function (done) {
-            bfx_ws.once('open', function () {
-                bfx_ws.auth(' ', ' ');
-                bfx_ws.auth(test_keys.standard.api_key, test_keys.standard.api_secret);
-                bfx_ws.subTicker();
-                bfx_ws.subTrades();
-                bfx_ws.subBook();
-                bfx_ws.subTicker("LTCUSD");
-                bfx_ws.subTrades("LTCUSD");
-                bfx_ws.subBook("LTCUSD");
-                bfx_ws.send(JSON.stringify({
-                    "event": "ping"
-                }));
-                bfx_ws.auth();
-                setTimeout(function () {
-                    done()
-                }, 15000)
-            });
-        });
-    it('should receive a pong', function () {
-        _.find(bfx_ws.messages, function (v) {
-            return v.event == 'pong'
-        });
-    });
-    it('should map all the channels', function () {
-        var values = Object.getOwnPropertyNames(bfx_ws.mapping).map(function (key) {
-            return bfx_ws.mapping[key];
-        });
-        expect(values).to.include.members(['BTCUSD_ticker', 'BTCUSD_trades', 'BTCUSD_book']);
-    });
-    it('should receive info message',
-        function () {
-            expect(bfx_ws.messages).is.not.empty;
-            expect(bfx_ws.messages.pop()).is.eql('ws opened...');
-            expect(bfx_ws.messages.pop()).is.eql({'event': 'info', 'version': 1});
-        });
-    it('should receive sub success messages', function () {
-        expect(bfx_ws.messages.filter(function (v) {
-            return v.event == 'subscribed'
-        }).length).is.eql(6)
-    });
-    it('the order snapshot should have the correct number of fields in the correct hierarchy', function () {
-        var chan = _.invert(bfx_ws.mapping)["BTCUSD_book"];
-        var book_snapshot = _.find(bfx_ws.messages.reverse(), function (v) {
-            return v[0] == chan
-        });
-        expect(book_snapshot[0]).is.a.number;
-        expect(book_snapshot[1]).is.an.array;
-        expect(book_snapshot[1][0]).is.an.array;
-        expect(book_snapshot[1][0].length).is.eql(3);
-        expect(_.every(book_snapshot[1][0], function (v) {
-            return _.isFinite(v)
-        })).ok
-    });
-    it('the types, structure and amount of order updates should be correct', function () {
-        var chan = _.invert(bfx_ws.mapping)["BTCUSD_book"];
-        var book_update = _.find(bfx_ws.messages.reverse(), function (v) {
-            return v[0] == chan
-        });
-        expect(_.every(book_update, function (v) {
-            return _.isFinite(v)
-        })).ok;
-        expect(book_update.length).is.eql(4);
-    });
+var expect = require('chai').expect;
+var BFX = require('../index');
+var _ = require('lodash');
+var test_keys = require('./keys.json');
 
-    it('the trades snapshot should have the correct number of fields in the correct hierarchy', function () {
-        var chan = _.invert(bfx_ws.mapping)["BTCUSD_trades"];
-        var trades_snapshot = _.find(bfx_ws.messages.reverse(), function (v) {
-            return v[0] == chan
-        });
-        expect(trades_snapshot[0]).is.a.number;
-        expect(trades_snapshot[1]).is.an.array;
-        expect(trades_snapshot[1][0]).is.an.array;
-        expect(trades_snapshot[1][0].length).is.eql(4);
-        expect(_.every(trades_snapshot[1][0], function (v) {
-            return _.isFinite(v)
-        })).ok
+describe('WebSocket', function () {
+  this.timeout(3000);
+  
+  beforeEach(function () {
+    var bitfinex = new BFX(test_keys.standard.api_key,
+      test_keys.standard.api_secret);
+    this.bitfinexWS = bitfinex.ws;
+    // this.bitfinexWS.on('open', done);
+  });
+  
+  afterEach(function (done) {
+    this.bitfinexWS.close();
+    this.bitfinexWS.on('close', function () {
+      done();
     });
-    it.skip('the types, structure and amount of trade updates should be correct', function () {
-        var chan = _.invert(bfx_ws.mapping)["BTCUSD_trades"];
-        var trades_update = _.find(bfx_ws.messages, function (v) {
-            return v[0] == chan
-        });
-        console.log(trades_update);
-        expect(_.every(trades_update, function (v) {
-            return _.isFinite(v)
-        })).ok;
-        expect(trades_update.length).is.eql(5);
+  });
+  
+  it('should receive a pong after a ping', function (done) {
+    this.bitfinexWS.on('pong', function () {
+      done();
     });
-    it("should request an api_key and api_secret if none is present", function () {
-        bfx_ws.api_key = null;
-        bfx_ws.api_secret = null;
-        expect(bfx_ws.auth).to.throw('need api_key and api_secret');
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.send({event: 'ping'});
+    }.bind(this));
+  });
+  
+  it('should receive a subscribed success messages', function (done) {
+    this.bitfinexWS.on('subscribed', function (data) {
+      expect(data).to.have.property('eventName', 'BTCUSD_trades');
+      expect(data).to.have.property('channel', 'trades');
+      expect(data).to.have.property('pair', 'BTCUSD');
+      done();
     });
-    it("should receive a rejection if the api_key and secret are not valid", function () {
-        expect(_.findWhere(bfx_ws.messages, {event: 'auth', status: 'FAILED', chanId: 0, code: 10100})).to.exist;
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeTrades('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('should receive a pong after a ping', function (done) {
+    this.bitfinexWS.on('pong', function () {
+      done();
     });
-    it("should authenticate", function () {
-        var auth_message = _.findWhere(bfx_ws.messages, {
-            event: 'auth', status: 'OK', chanId: 0, userId: 163319
-        });
-        expect(auth_message).to.exist
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.send({event: 'ping'});
+    }.bind(this));
+  });
+  
+  it('should map trade channel', function (done) {
+    this.bitfinexWS.once('subscribed', function () {
+      var channels = Object.getOwnPropertyNames(this.bitfinexWS.channelMap)
+        .map(function (channelId) {
+          return this.bitfinexWS.channelMap[channelId].eventName;
+        }.bind(this));
+      expect(channels).to.include('BTCUSD_trades');
+      done();
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeTrades('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('should map ticker channel', function (done) {
+    this.bitfinexWS.once('subscribed', function () {
+      var channels = Object.getOwnPropertyNames(this.bitfinexWS.channelMap)
+        .map(function (channelId) {
+          return this.bitfinexWS.channelMap[channelId].eventName;
+        }.bind(this));
+      expect(channels).to.include('BTCUSD_ticker');
+      done();
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeTicker('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('should map book channel', function (done) {
+    this.bitfinexWS.once('subscribed', function () {
+      var channels = Object.getOwnPropertyNames(this.bitfinexWS.channelMap)
+        .map(function (channelId) {
+          return this.bitfinexWS.channelMap[channelId].eventName;
+        }.bind(this));
+      expect(channels).to.include('BTCUSD_book');
+      done();
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeOrderBook('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('should receive info message', function (done) {
+    this.bitfinexWS.on('info', function (data) {
+      expect(data).is.eql({
+        event: 'info', 
+        version: 1
+      });
+      done();
     });
-    it('should give error if already authenticated', function () {
-        var auth_error = _.findWhere(bfx_ws.messages, {
-            event: 'error', msg: 'already authenticated', code: 10100
-        });
-        expect(auth_error).to.exist
-    });
-    it('should receive an order snapshot', function () {
-        var os = _.filter(bfx_ws.messages, function (v) {
-            v[0] = 0;
-            v[1] = "os";
-        });
-        expect(os).to.exist;
-    });
-    it('should receive a wallet balance snapshot', function () {
-        var os = _.filter(bfx_ws.messages, function (v) {
-            v[0] = 0;
-            v[1] = "ws";
-        });
-        expect(os).to.exist;
-    });
-    it('should receive a trade history snapshot', function () {
-        var os = _.filter(bfx_ws.messages, function (v) {
-            v[0] = 0;
-            v[1] = "ts";
-        });
-        expect(os).to.exist;
-    });
-    it('should receive a position snapshot', function () {
-        var os = _.filter(bfx_ws.messages, function (v) {
-            v[0] = 0;
-            v[1] = "ps";
-        });
-        expect(os).to.exist;
-    });
-    it('unsubscribing',
-        function (done) {
-            bfx_ws.unSubTickerPair("LTCUSD");
-            bfx_ws.unSubTradesPair("LTCUSD");
-            bfx_ws.unSubBookPair("LTCUSD");
-            bfx_ws.unSubTickerPair();
-            bfx_ws.unSubTradesPair();
-            bfx_ws.unSubBookPair();
-            setTimeout(function () {
-                done()
-            }, 2000)
-        });
-    it('should disconnect', function () {
-        bfx_ws.close();
-        var close_message = _.find(bfx_ws.messages, function (v) {
-            return v = 'ws closed...'
-        });
-        expect(close_message).to.exist;
-    });
-    it('should log messages onclose and onerror', function (done) {
-        bfx_ws.onclose('test');
-        bfx_ws.onerror('test');
+  });
+  
+  
+  it('#orderBook data should have the corresponding fields', function(done) {
+    this.bitfinexWS.once('BTCUSD_book', function (data) {
+      expect(data.price).to.be.a('number');
+      expect(data.count).to.be.a('number');
+      expect(data.amount).to.be.a('number');
+      done();
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeOrderBook('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('#trade data should have the corresponding fields', function(done) {
+    this.bitfinexWS.once('BTCUSD_trades', function (data) {
+      expect(data.seq).to.be.a('string');
+      expect(data.timestamp).to.be.a('number');
+      expect(data.price).to.be.a('number');
+      expect(data.amount).to.be.a('number');
+      done();
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeTrades('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('#ticker data should have the corresponding fields', function(done) {
+    this.bitfinexWS.once('BTCUSD_ticker', function (data) {
+      expect(data.bid).to.be.a('number');
+      expect(data.bidSize).to.be.a('number');
+      expect(data.ask).to.be.a('number');
+      expect(data.askSize).to.be.a('number');
+      expect(data.dailyChange).to.be.a('number');
+      expect(data.dailyChangePerc).to.be.a('number');
+      expect(data.lastPrice).to.be.a('number');
+      expect(data.volume).to.be.a('number');
+      expect(data.high).to.be.a('number');
+      expect(data.low).to.be.a('number');
+      done();
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeTicker('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('should unsubscribe by channelId', function (done) {
+    this.bitfinexWS.once('subscribed', function (data) {
+      var channelId = data.chanId;
+      this.bitfinexWS.once('unsubscribed', function (data) {
+        expect(data.status).to.equal('OK');
+        expect(data.chanId).to.equal(channelId);
         done();
+      });
+      this.bitfinexWS.unsubscribe(channelId);
+    }.bind(this));
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.subscribeTicker('BTCUSD');
+    }.bind(this));
+  });
+  
+  it('should emit an auth event when authorized', function (done) {
+    this.bitfinexWS.on('auth', function (data) {
+      console.log(data);
+      expect(data.status).to.equal('OK');
+      done();
     });
-    it('should handle errors properly', function () {
-        var error = function () {
-            bfx_ws.send(JSON.stringify({
-                "event": "ping"
-            }))
-        };
-        expect(error).to.throw(Error)
-    })
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.auth();
+    }.bind(this));
+  });
+  
+  it('should emit an error when authorization fails', function (done) {
+    this.bitfinexWS.APIKey = '';
+    this.bitfinexWS.APISecret = '';
+    
+    this.bitfinexWS.on('error', function (error) {
+      done();
+    });
+    
+    this.bitfinexWS.on('open', function () {
+      this.bitfinexWS.auth();
+    }.bind(this));
+  });
+  
 });
