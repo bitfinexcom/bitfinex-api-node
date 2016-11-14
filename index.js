cee7ec4765718e51815b9160b5951dedab8fa0b6
joshuarossi <joshua.rossi@icloud.com>
2016-11-14 16:43:57 -0500

    added placeholders for v2

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-"use strict";
+"use strict"
 
-var APIRest = require('./rest.js');
-var WS = require('./ws.js');
+var APIRest = require('./rest.js')
+var WS = require('./ws.js')
 
-var BFX = function (api_key, api_secret){
-    this.api_key = api_key;
-    this.api_secret = api_secret;
-    this.rest = new APIRest(this.api_key, this.api_secret);
-    this.ws = new WS(this.api_key, this.api_secret);
-};
+var BFX = function (api_key, api_secret, version){
+    this.api_key = api_key
+    this.api_secret = api_secret
+    if (version == 2){
+    	this.rest = new Rest2(this.api_key, this.api_secret)
+    	this.ws = new WS2(this.api_key, this.api_secret)
+    }
+    else {
+    	this.rest = new APIRest(this.api_key, this.api_secret, version)
+    this.ws = new WS(this.api_key, this.api_secret, version)
+    }
+    
+}
 
-BFX.WS = WS;
-BFX.APIRest = APIRest;
+BFX.WS = WS
+BFX.APIRest = APIRest
 
 module.exports = BFX;
---
+++ rest2.js
@@ -0,0 +1,90 @@
+class Rest2 {
+  constructor(key, secret, nonceGenerator) {
+    this.url = "https://api.bitfinex.com"
+    this.version = 'v2'
+    this.key = key
+    this.secret = secret
+    this.nonce = new Date().getTime()
+    this._nonce = typeof nonceGenerator === "function" ? nonceGenerator : function() {
+      //noinspection JSPotentiallyInvalidUsageOfThis
+      return ++this.nonce
+    }
+  }
+
+  make_request(sub_path, params, cb) {
+    var headers, key, nonce, path, payload, signature, url, value
+    if (!this.key || !this.secret) {
+      return cb(new Error("missing api key or secret"));
+    }
+    path = '/' + this.version + '/' + sub_path;
+    url = this.url + path;
+    nonce = JSON.stringify(this._nonce());
+    payload = {
+      request: path,
+      nonce: nonce
+    };
+    for (key in params) {
+      value = params[key];
+      payload[key] = value;
+    }
+    payload = new Buffer(JSON.stringify(payload)).toString('base64');
+    signature = crypto.createHmac("sha384", this.secret).update(payload).digest('hex');
+    headers = {
+      'X-BFX-APIKEY': this.key,
+      'X-BFX-PAYLOAD': payload,
+      'X-BFX-SIGNATURE': signature
+    }
+    return request({
+      url: url,
+      method: "POST",
+      headers: headers,
+      timeout: 15000
+    }, function(err, response, body) {
+      var error, error1, result;
+      if (err || (response.statusCode !== 200 && response.statusCode !== 400)) {
+        return cb(new Error(err != null ? err : response.statusCode));
+      }
+      try {
+        result = JSON.parse(body);
+      } catch (error1) {
+        error = error1;
+        return cb(null, {
+          message: body.toString()
+        });
+      }
+      if (result.message != null) {
+        return cb(new Error(result.message));
+      }
+      return cb(null, result);
+    })
+  }
+
+  make_public_request(path, cb) {
+    var url;
+    path = '/' + this.version + '/' + sub_path;
+    url = this.url + path;
+    return request({
+      url: url,
+      method: "GET",
+      timeout: 15000
+    }, function(err, response, body) {
+      var error, error1, result;
+      if (err || (response.statusCode !== 200 && response.statusCode !== 400)) {
+        return cb(new Error(err != null ? err : response.statusCode));
+      }
+      try {
+        result = JSON.parse(body);
+      } catch (error1) {
+        error = error1;
+        return cb(null, {
+          message: body.toString()
+        });
+      }
+      if (result.message != null) {
+        return cb(new Error(result.message));
+      }
+      return cb(null, result);
+    });
+  }
+
+}
