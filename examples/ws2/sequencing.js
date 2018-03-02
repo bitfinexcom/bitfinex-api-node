a32ec61707a8c2e7a4e87240d630719842970a24
Cris Mihalache <me@f3rno.com>
2018-03-02 14:00:55 +0100

    (refactor) sequencing example to show the seq #s

--- examples/ws2/sequencing.js
+++ examples/ws2/sequencing.js
@@ -1,5 +1,7 @@
 'use strict'
 
+process.env.DEBUG = 'bfx:examples:*'
+
 const debug = require('debug')('bfx:examples:ws2_sequencing')
 const bfx = require('../bfx')
 const ws = bfx.ws(2, { seqAudit: true })
@@ -10,14 +12,34 @@ ws.on('open', () => {
   ws.enableSequencing()
   ws.subscribeTrades('tBTCUSD')
 
+  ws.auth()
+
   ws.on('message', (msg) => {
-    debug('msg: %j', msg)
+    if (!Array.isArray(msg)) return // only array messages have sequence #s
+
+    const authSeq = msg[0] === 0 && msg[1] !== 'hb'
+      ? msg[msg.length - 1]
+      : NaN
+
+    const seq = msg[0] === 0 && msg[1] !== 'hb'
+      ? msg[msg.length - 2]
+      : msg[msg.length - 1]
+
+    if (Number.isNaN(authSeq)) {
+      debug('recv public seq # %d', seq)
+    } else {
+      debug('recv public seq # %d, auth seq # %d', seq, authSeq)
+    }
   })
 })
 
+ws.on('auth', () => {
+  debug('authenticated')
+})
+
 // An error will emit on an invalid seq #
-ws.on('error', (err) => {
-  debug('error: %j', err)
+ws.on('error', err => {
+  debug('error: %s', err.message)
 })
 
 ws.open()
