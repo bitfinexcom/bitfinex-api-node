08efd0aba2c6ca308c74271bfbc7ba0cb42eb4b6
Cris Mihalache <me@f3rno.com>
2018-04-11 20:16:16 +0200

    (feature) add more comments to seq example, show audit flag

--- examples/ws2/sequencing.js
+++ examples/ws2/sequencing.js
@@ -9,7 +9,9 @@ const ws = bfx.ws(2)
 ws.on('open', () => {
   debug('connection opened')
 
-  ws.enableSequencing().then(() => {
+  // Enables internal sequence tracking; an error will be emitted if there is a
+  // seq # mis-match
+  ws.enableSequencing({ audit: true }).then(() => {
     if (!ws.isFlagEnabled(65536)) {
       throw new Error('seq enable succeeded, but flag not updated')
     }
@@ -25,10 +27,12 @@ ws.on('open', () => {
   ws.on('message', (msg) => {
     if (!Array.isArray(msg)) return // only array messages have sequence #s
 
+    // auth seq number, available as the last element on chan 0 packets
     const authSeq = msg[0] === 0 && msg[1] !== 'hb'
       ? msg[msg.length - 1]
       : NaN
 
+    // public seq number, last or 2nd to last element on all packets
     const seq = msg[0] === 0 && msg[1] !== 'hb'
       ? msg[msg.length - 2]
       : msg[msg.length - 1]
