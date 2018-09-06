67059bf438ca183b62005cd9435c38ad43279f2b
Cris Mihalache <me@f3rno.com>
2018-09-06 15:02:02 +0300

    (refactor) rm unecessary imports

--- examples/plugins/wd.js
+++ examples/plugins/wd.js
@@ -4,10 +4,6 @@ process.env.DEBUG = 'bfx:api:plugins:*,bfx:api:examples:*'
 
 const debug = require('debug')('bfx:api:examples:ws2:plugin-wd')
 const { Manager, subscribe } = require('bfx-api-node-core')
-
-const {
-  Config, enableFlag, disableFlag, isFlagEnabled,
-} = require('bfx-api-node-core')
 const WDPlugin = require('bfx-api-node-plugin-wd')
 const managerArgs = require('../manager_args')
 
