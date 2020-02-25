b4e7ee9137434a14676a1b8838740a2fc876afd5
Cris Mihalache <me@f3rno.com>
2020-02-25 23:10:18 +0700

    (fix) fix tests so they can run concurrently along all other HF/BFX API projects

--- test/lib/transports/ws2-integration.js
+++ test/lib/transports/ws2-integration.js
@@ -24,83 +24,99 @@ const createTestWSv2Instance = (params = {}) => {
   })
 }
 
-describe('WSv2 orders', () => {
-  it('creates & confirms orders', async () => {
-    const wss = new MockWSv2Server({ listen: true })
-    const ws = createTestWSv2Instance()
+describe('WSv2 integration', () => {
+  let ws = null
+  let wss = null
 
-    await ws.open()
-    await ws.auth()
+  afterEach(async () => {
+    try { // may fail due to being modified by a test, it's not a problem
+      if (ws && ws.isOpen()) {
+        await ws.close()
+      }
+    } catch (e) {}
 
-    const o = new Order({
-      gid: null,
-      cid: 0,
-      type: 'EXCHANGE LIMIT',
-      price: 100,
-      amount: 1,
-      symbol: 'tBTCUSD'
-    })
+    if (wss && wss.isOpen()) {
+      await wss.close()
+    }
 
-    return ws.submitOrder(o).then(() => {
-      wss.close()
-    })
+    ws = null
+    wss = null
   })
 
-  it('keeps orders up to date', async () => {
-    const wss = new MockWSv2Server({ listen: true })
-    const ws = createTestWSv2Instance()
+  describe('orders', () => {
+    it('creates & confirms orders', async () => {
+      wss = new MockWSv2Server({ listen: true })
+      ws = createTestWSv2Instance()
 
-    await ws.open()
-    await ws.auth()
+      await ws.open()
+      await ws.auth()
 
-    const o = new Order({
-      gid: null,
-      cid: 0,
-      type: 'EXCHANGE LIMIT',
-      price: 100,
-      amount: 1,
-      symbol: 'tBTCUSD'
-    }, ws)
+      const o = new Order({
+        gid: null,
+        cid: 0,
+        type: 'EXCHANGE LIMIT',
+        price: 100,
+        amount: 1,
+        symbol: 'tBTCUSD'
+      })
 
-    o.registerListeners()
+      return ws.submitOrder(o)
+    })
 
-    await o.submit()
+    it('keeps orders up to date', async () => {
+      wss = new MockWSv2Server({ listen: true })
+      ws = createTestWSv2Instance()
 
-    const arr = o.serialize()
-    arr[16] = 256
+      await ws.open()
+      await ws.auth()
 
-    wss.send([0, 'ou', arr])
+      const o = new Order({
+        gid: null,
+        cid: 0,
+        type: 'EXCHANGE LIMIT',
+        price: 100,
+        amount: 1,
+        symbol: 'tBTCUSD'
+      }, ws)
 
-    await delay(100)
+      o.registerListeners()
 
-    assert.strictEqual(o.price, 256)
-    arr[16] = 150
+      await o.submit()
 
-    wss.send([0, 'oc', arr])
+      const arr = o.serialize()
+      arr[16] = 256
 
-    await delay(100)
+      wss.send([0, 'ou', arr])
 
-    assert.strictEqual(o.price, 150)
-    o.removeListeners()
-    wss.close()
-  })
+      await delay(100)
 
-  it('updateOrder: sends order changeset packet through', async () => {
-    const wss = new MockWSv2Server()
-    const ws = createTestWSv2Instance()
+      assert.strictEqual(o.price, 256)
+      arr[16] = 150
 
-    await ws.open()
-    await ws.auth()
+      wss.send([0, 'oc', arr])
 
-    const o = new Order({
-      id: Date.now(),
-      type: 'EXCHANGE LIMIT',
-      price: 100,
-      amount: 1,
-      symbol: 'tBTCUSD'
-    }, ws)
+      await delay(100)
 
-    return new Promise((resolve) => {
+      assert.strictEqual(o.price, 150)
+      o.removeListeners()
+    })
+
+    it('updateOrder: sends order changeset packet through', async () => {
+      wss = new MockWSv2Server()
+      ws = createTestWSv2Instance()
+
+      await ws.open()
+      await ws.auth()
+
+      let sawMessage = false
+      const o = new Order({
+        id: Date.now(),
+        type: 'EXCHANGE LIMIT',
+        price: 100,
+        amount: 1,
+        symbol: 'tBTCUSD'
+      }, ws)
+
       ws._ws.send = (msgJSON) => {
         const msg = JSON.parse(msgJSON)
 
@@ -111,191 +127,193 @@ describe('WSv2 orders', () => {
         assert.strictEqual(+msg[3].delta, 1)
         assert.strictEqual(+msg[3].price, 200)
 
-        wss.close()
-        resolve()
+        sawMessage = true
       }
 
-      o.update({ price: 200, delta: 1 })
-    })
-  })
-
-  it('sends individual order packets when not buffering', async () => {
-    const wss = new MockWSv2Server()
-    const ws = createTestWSv2Instance()
-
-    await ws.open()
-    await ws.auth()
-
-    const oA = new Order({
-      gid: null,
-      cid: Date.now(),
-      type: 'EXCHANGE LIMIT',
-      price: 100,
-      amount: 1,
-      symbol: 'tBTCUSD'
+      o.update({ price: 200, delta: 1 }) // note promise ignored
+      assert(sawMessage)
     })
 
-    const oB = new Order({
-      gid: null,
-      cid: Date.now(),
-      type: 'EXCHANGE LIMIT',
-      price: 10,
-      amount: 1,
-      symbol: 'tETHUSD'
-    })
+    it('sends individual order packets when not buffering', async () => {
+      wss = new MockWSv2Server()
+      ws = createTestWSv2Instance()
 
-    let sendN = 0
+      await ws.open()
+      await ws.auth()
 
-    return new Promise(async (resolve) => {
+      let sawBothOrders = false
+      const oA = new Order({
+        gid: null,
+        cid: Date.now(),
+        type: 'EXCHANGE LIMIT',
+        price: 100,
+        amount: 1,
+        symbol: 'tBTCUSD'
+      })
+
+      const oB = new Order({
+        gid: null,
+        cid: Date.now(),
+        type: 'EXCHANGE LIMIT',
+        price: 10,
+        amount: 1,
+        symbol: 'tETHUSD'
+      })
+
+      let sendN = 0
+
       ws._ws.send = (msgJSON) => {
         const msg = JSON.parse(msgJSON)
         assert.strictEqual(msg[1], 'on')
         sendN++
 
         if (sendN === 2) {
-          wss.close()
-          resolve()
+          sawBothOrders = true
         }
       }
 
       // note promises ignored
       ws.submitOrder(oA)
       ws.submitOrder(oB)
-    })
-  })
 
-  it('buffers order packets', async () => {
-    const wss = new MockWSv2Server()
-    const ws = createTestWSv2Instance({
-      orderOpBufferDelay: 100
+      assert(sawBothOrders)
     })
 
-    await ws.open()
-    await ws.auth()
+    it('buffers order packets', async () => {
+      wss = new MockWSv2Server()
+      ws = createTestWSv2Instance({
+        orderOpBufferDelay: 100
+      })
 
-    const oA = new Order({
-      gid: null,
-      cid: Date.now(),
-      type: 'EXCHANGE LIMIT',
-      price: 100,
-      amount: 1,
-      symbol: 'tBTCUSD'
-    })
+      await ws.open()
+      await ws.auth()
 
-    const oB = new Order({
-      gid: null,
-      cid: Date.now(),
-      type: 'EXCHANGE LIMIT',
-      price: 10,
-      amount: 1,
-      symbol: 'tETHUSD'
-    })
+      const oA = new Order({
+        gid: null,
+        cid: Date.now(),
+        type: 'EXCHANGE LIMIT',
+        price: 100,
+        amount: 1,
+        symbol: 'tBTCUSD'
+      })
 
-    return new Promise(async (resolve) => {
-      ws._ws.send = (msgJSON) => {
-        const msg = JSON.parse(msgJSON)
-        assert.strictEqual(msg[1], 'ox_multi')
+      const oB = new Order({
+        gid: null,
+        cid: Date.now(),
+        type: 'EXCHANGE LIMIT',
+        price: 10,
+        amount: 1,
+        symbol: 'tETHUSD'
+      })
 
-        msg[3].forEach((payload) => {
-          assert.strictEqual(payload[0], 'on')
-        })
+      return new Promise((resolve) => {
+        ws._ws.send = (msgJSON) => {
+          const msg = JSON.parse(msgJSON)
+          assert.strictEqual(msg[1], 'ox_multi')
 
-        wss.close()
-        resolve()
-      }
+          msg[3].forEach((payload) => {
+            assert.strictEqual(payload[0], 'on')
+          })
 
-      // note promises ignored
-      ws.submitOrder(oA)
-      ws.submitOrder(oB)
-    })
-  })
-})
+          wss.close()
+          resolve()
+        }
 
-describe('WSv2 listeners', () => {
-  it('manages listeners by cbGID', () => {
-    const ws = createTestWSv2Instance()
-    ws._channelMap = { 0: { channel: 'auth' } }
-
-    let updatesSeen = 0
-    ws.onAccountTradeUpdate({ pair: 'BTCUSD', cbGID: 10 }, () => updatesSeen++)
-    ws.onOrderUpdate({ symbol: 'tBTCUSD', cbGID: 10 }, () => updatesSeen++)
-
-    ws._handleChannelMessage([0, 'tu', [123, 'tBTCUSD']])
-    ws._handleChannelMessage([0, 'ou', [0, 0, 0, 'tBTCUSD']])
-    ws.removeListeners(10)
-    ws._handleChannelMessage([0, 'tu', [123, 'tBTCUSD']])
-    ws._handleChannelMessage([0, 'ou', [0, 0, 0, 'tBTCUSD']])
-
-    assert.strictEqual(updatesSeen, 2)
-  })
-
-  it('tracks channel refs to auto sub/unsub', async () => {
-    const ws = createTestWSv2Instance()
-    const wss = new MockWSv2Server()
-    let subs = 0
-    let unsubs = 0
-
-    await ws.open()
-
-    wss.on('message', (ws, msg) => {
-      if (msg.event === 'subscribe' && msg.channel === 'trades') {
-        subs++
-        ws.send(JSON.stringify({
-          event: 'subscribed',
-          chanId: 42,
-          channel: 'trades',
-          symbol: msg.symbol
-        }))
-      } else if (msg.event === 'unsubscribe' && msg.chanId === 42) {
-        unsubs++
-        ws.send(JSON.stringify({
-          event: 'unsubscribed',
-          chanId: 42
-        }))
-      }
-    })
-
-    ws.subscribeTrades('tBTCUSD')
-    ws.subscribeTrades('tBTCUSD')
-    ws.subscribeTrades('tBTCUSD')
-
-    ws.on('subscribed', () => {
-      ws.unsubscribeTrades('tBTCUSD')
-      ws.unsubscribeTrades('tBTCUSD')
-      ws.unsubscribeTrades('tBTCUSD')
-      ws.unsubscribeTrades('tBTCUSD')
-      ws.unsubscribeTrades('tBTCUSD')
-    })
-
-    return new Promise((resolve) => {
-      ws.on('unsubscribed', () => {
-        assert.strictEqual(subs, 1)
-        assert.strictEqual(unsubs, 1)
-        wss.close()
-        resolve()
+        // note promises ignored
+        ws.submitOrder(oA)
+        ws.submitOrder(oB)
       })
     })
   })
-})
 
-describe('WSv2 info message handling', () => {
-  it('notifies listeners on matching code', (done) => {
-    const ws = new WSv2()
+  describe('listeners', () => {
+    it('manages listeners by cbGID', () => {
+      ws = createTestWSv2Instance()
+      ws._channelMap = { 0: { channel: 'auth' } }
 
-    ws.onInfoMessage(WSv2.info.MAINTENANCE_END, () => {
-      done()
+      let updatesSeen = 0
+      ws.onAccountTradeUpdate({ pair: 'BTCUSD', cbGID: 10 }, () => updatesSeen++)
+      ws.onOrderUpdate({ symbol: 'tBTCUSD', cbGID: 10 }, () => updatesSeen++)
+
+      ws._handleChannelMessage([0, 'tu', [123, 'tBTCUSD']])
+      ws._handleChannelMessage([0, 'ou', [0, 0, 0, 'tBTCUSD']])
+      ws.removeListeners(10)
+      ws._handleChannelMessage([0, 'tu', [123, 'tBTCUSD']])
+      ws._handleChannelMessage([0, 'ou', [0, 0, 0, 'tBTCUSD']])
+
+      assert.strictEqual(updatesSeen, 2)
     })
 
-    ws._onWSMessage(JSON.stringify({
-      event: 'info',
-      code: WSv2.info.MAINTENANCE_START,
-      msg: ''
-    }))
+    it('tracks channel refs to auto sub/unsub', async () => {
+      ws = createTestWSv2Instance()
+      wss = new MockWSv2Server()
+      let subs = 0
+      let unsubs = 0
 
-    ws._onWSMessage(JSON.stringify({
-      event: 'info',
-      code: WSv2.info.MAINTENANCE_END,
-      msg: ''
-    }))
+      await ws.open()
+
+      wss.on('message', (ws, msg) => {
+        if (msg.event === 'subscribe' && msg.channel === 'trades') {
+          subs++
+          ws.send(JSON.stringify({
+            event: 'subscribed',
+            chanId: 42,
+            channel: 'trades',
+            symbol: msg.symbol
+          }))
+        } else if (msg.event === 'unsubscribe' && msg.chanId === 42) {
+          unsubs++
+          ws.send(JSON.stringify({
+            event: 'unsubscribed',
+            chanId: 42
+          }))
+        }
+      })
+
+      ws.subscribeTrades('tBTCUSD')
+      ws.subscribeTrades('tBTCUSD')
+      ws.subscribeTrades('tBTCUSD')
+
+      ws.on('subscribed', () => {
+        ws.unsubscribeTrades('tBTCUSD')
+        ws.unsubscribeTrades('tBTCUSD')
+        ws.unsubscribeTrades('tBTCUSD')
+        ws.unsubscribeTrades('tBTCUSD')
+        ws.unsubscribeTrades('tBTCUSD')
+      })
+
+      return new Promise((resolve) => {
+        ws.on('unsubscribed', () => {
+          assert.strictEqual(subs, 1)
+          assert.strictEqual(unsubs, 1)
+          resolve()
+        })
+      })
+    })
+  })
+
+  describe('info message handling', () => {
+    it('notifies listeners on matching code', () => {
+      let sawMaintenanceEnd = false
+      ws = new WSv2()
+
+      ws.onInfoMessage(WSv2.info.MAINTENANCE_END, () => {
+        sawMaintenanceEnd = true
+      })
+
+      ws._onWSMessage(JSON.stringify({
+        event: 'info',
+        code: WSv2.info.MAINTENANCE_START,
+        msg: ''
+      }))
+
+      ws._onWSMessage(JSON.stringify({
+        event: 'info',
+        code: WSv2.info.MAINTENANCE_END,
+        msg: ''
+      }))
+
+      assert(sawMaintenanceEnd)
+    })
   })
 })
