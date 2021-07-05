f68bf6c2ead25cfea7c1c04d8fdbfe1dc5f7bbd8
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-07-05 20:27:52 +0300

    update 'notifications' example

--- examples/ws2/notifications.js
+++ examples/ws2/notifications.js
@@ -1,12 +1,21 @@
 'use strict'
 
-const runExample = require('../util/run_example')
+const { args: { apiKey, apiSecret }, debug } = require('../util/setup')
+const WSv2 = require('../../lib/transports/ws2')
 
-module.exports = runExample({
-  name: 'ws2-notifications',
-  ws: { env: true, connect: true, auth: true, transform: true }
-}, async ({ ws, debug }) => {
+async function execute () {
+  const ws = new WSv2({
+    apiKey,
+    apiSecret,
+    transform: true
+  })
+  ws.on('error', e => debug('WSv2 error: %s', e.message | e))
+  await ws.open()
+  await ws.auth()
+
   ws.onNotification({ type: '*' }, (n) => {
     debug('recv notification: %j', n.toJS())
   })
-})
+}
+
+execute()
