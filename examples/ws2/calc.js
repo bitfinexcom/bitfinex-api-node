5e449b3cdb980e7d5ef70131116c65ba750ab004
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-07-05 17:15:27 +0300

    calc example

--- examples/ws2/calc.js
+++ examples/ws2/calc.js
@@ -1,12 +1,24 @@
 'use strict'
 
 const Promise = require('bluebird')
-const runExample = require('../util/run_example')
+const dotenv = require('dotenv')
+const D = require('debug')
+const WSv2 = require('../../lib/transports/ws2')
+const debug = D('>')
+debug.enabled = true
 
-module.exports = runExample({
-  name: 'ws2-calc',
-  ws: { env: true, connect: true, auth: true }
-}, async ({ ws, debug }) => {
+dotenv.config()
+const { API_KEY, API_SECRET } = process.env
+
+async function execute () {
+  const ws = new WSv2({
+    apiKey: API_KEY,
+    apiSecret: API_SECRET
+  })
+  ws.on('error', e => debug('WSv2 error: %s', e.message | e))
+  await ws.open()
+  await ws.auth()
+
   await Promise.delay(5 * 1000)
 
   ws.requestCalc([
@@ -17,7 +29,10 @@ module.exports = runExample({
   ])
 
   // Watch log output for balance update packets (wu, miu, etc)
-  debug('sent calc, closing in 30s...')
+  debug('sent calc, closing in 3s...')
 
-  await Promise.delay(30 * 1000)
-})
+  await Promise.delay(3 * 1000)
+  await ws.close()
+}
+
+execute()
