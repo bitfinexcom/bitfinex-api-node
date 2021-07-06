addaa38f81a6b8e4a0ab9c9513fb64c0ca01914d
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-07-06 21:16:24 +0300

    add debugTable into setup

--- examples/util/setup.js
+++ examples/util/setup.js
@@ -3,17 +3,38 @@
 const dotenv = require('dotenv')
 const Readline = require('readline-promise').default
 const argsFromEnv = require('./args_from_env')
+const debugTableUtil = require('./debug_table')
 const D = require('./debug').get()
 const debug = D('>')
 debug.enabled = true
 
 dotenv.config()
 
+/**
+ * Log a table to the console
+ *
+ * @param {object} args - arguments
+ * @param {object[]} args.rows - data, can be specified as 2nd param
+ * @param {string[]} args.headers - column labels
+ * @param {number[]} args.widths - column widths
+ * @param {object[]} extraRows - optional row spec as 2nd param
+ */
+const debugTable = ({ rows = [], headers, widths }, extraRows = []) => {
+  debug('')
+  debugTableUtil({
+    rows: [...rows, ...extraRows],
+    headers,
+    widths,
+    debug
+  })
+  debug('')
+}
 module.exports = {
   get args () {
     return argsFromEnv()
   },
   debug,
+  debugTable,
   get readline () {
     return Readline.createInterface({
       input: process.stdin,
