0e913af48a394e26eb430508a217259b87a23626
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-07-07 16:16:52 +0300

    update 'ledgers' example

--- examples/rest2/ledgers.js
+++ examples/rest2/ledgers.js
@@ -1,16 +1,19 @@
 'use strict'
 
 const { prepareAmount } = require('bfx-api-node-util')
-const runExample = require('../util/run_example')
+const { RESTv2 } = require('bfx-api-node-rest')
+const { args: { apiKey, apiSecret }, debug, debugTable } = require('../util/setup')
 const argFromCLI = require('../util/arg_from_cli')
 
-module.exports = runExample({
-  name: 'rest-get-ledgers',
-  rest: { env: true, transform: true },
-  params: {
+async function execute () {
+  const rest = new RESTv2({
+    apiKey,
+    apiSecret,
+    transform: true
+  })
+  const params = {
     ccy: argFromCLI(0, 'all')
   }
-}, async ({ debug, debugTable, rest, params }) => {
   const ccy = params.ccy === 'all' ? null : params.ccy
 
   debug('fetching ledger entries for %s...', ccy || 'all currencies')
@@ -27,4 +30,6 @@ module.exports = runExample({
       'Entry ID', 'Currency', 'Timestamp', 'Amount', 'Balance', 'Description'
     ]
   })
-})
+}
+
+execute()
