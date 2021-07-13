2efb210420016c5e7d44efec4261913c1cae83fe
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-07-13 11:54:06 +0300

    remove api keys in 'tickers' example

--- examples/ws2/tickers.js
+++ examples/ws2/tickers.js
@@ -1,12 +1,10 @@
 'use strict'
 
-const { args: { apiKey, apiSecret }, debug } = require('../util/setup')
+const { debug } = require('../util/setup')
 const WSv2 = require('../../lib/transports/ws2')
 
 async function execute () {
   const ws = new WSv2({
-    apiKey,
-    apiSecret,
     transform: true
   })
   ws.on('error', e => debug('WSv2 error: %s', e.message | e))
